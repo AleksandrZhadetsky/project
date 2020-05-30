3ce7723f7ca8cc69e2123db0e2a8d5817f413809
Alexander Zhadetskij <coolweak2@gmail.com>
2020-05-30 21:25:03 +0300

    added external login authentication

--- crowdfunding/Controllers/AccountController.cs
+++ crowdfunding/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace crowdfunding.Controllers
@@ -77,10 +78,93 @@ namespace crowdfunding.Controllers
         }
 
         [AllowAnonymous]
-        [HttpGet]
-        public IActionResult Authenticate(string returnUrl = null)
+        [HttpPost]
+        public IActionResult ExternalLogin(string provider, string returnUrl)
         {
-            return View("Login");
+            var redirectUrl = Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl });
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+
+            return new ChallengeResult(provider, properties);
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> ExternalLoginCallback(string returnUrl = null, string remoteError = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            LoginModel model = new LoginModel
+            {
+                ReturnUrl = returnUrl,
+                ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+            };
+
+            if (remoteError != null)
+            {
+                ModelState.AddModelError(string.Empty, $"Error from external provider: {remoteError}");
+
+                return View("Login", model);
+            }
+
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+            {
+                ModelState.AddModelError(string.Empty, $"Error while loading external login information: {remoteError}");
+
+                return View("Login", model);
+            }
+
+            var signInResult =
+                await _signInManager.ExternalLoginSignInAsync(
+                info.LoginProvider,
+                info.ProviderKey,
+                isPersistent: false,
+                bypassTwoFactor: true);
+
+            if (signInResult.Succeeded)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                var email = info.Principal.FindFirstValue(ClaimTypes.Email);
+                if (email != null)
+                {
+                    var user = await _userManager.FindByEmailAsync(email);
+
+                    if (user == null)
+                    {
+                        user = new User
+                        {
+                            UserName = info.Principal.FindFirstValue(ClaimTypes.Email),
+                            Email = info.Principal.FindFirstValue(ClaimTypes.Email)
+                        };
+
+                        await _userManager.CreateAsync(user);
+                    }
+
+                    await _userManager.AddLoginAsync(user, info);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return LocalRedirect(returnUrl);
+                }
+            }
+
+            ViewBag.ErrorTitle = $"Email clain not received from: {info.LoginProvider}";
+            ViewBag.ErrorTitle = $"Please, contact support";
+
+            return View("Login", model);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Authenticate(string returnUrl = null)
+        {
+            LoginModel model = new LoginModel
+            {
+                ReturnUrl = returnUrl,
+                ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+            };
+            return View("Login", model);
         }
 
         [AllowAnonymous]
--- crowdfunding/Models/LoginModel.cs
+++ crowdfunding/Models/LoginModel.cs
@@ -1,9 +1,14 @@
-﻿namespace crowdfunding.Models
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Collections.Generic;
+
+namespace crowdfunding.Models
 {
     public class LoginModel
     {
         public string UserName { get; set; }
         public string Password { get; set; }
         public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
     }
 }
